df5ca3ee82d3bdc569caf3d05d3cfc28e045f430
Jan Brinkmann <jan@jbcommerce.de>
2021-03-21 18:35:45 +0100

    admin controller scaffold

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spod\Sync\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Spod_Sync::dashboard');
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
