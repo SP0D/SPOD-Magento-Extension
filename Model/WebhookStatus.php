fdb9f1173bea561d01cf81097974a032e40a63bb
Jan Brinkmann <jan@jbcommerce.de>
2021-03-23 10:43:51 +0100

    meta class for webhook event status mapping

---
+++ Model/WebhookStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Spod\Sync\Model;
+
+class WebhookStatus
+{
+    const WEBHOOK_STATUS_PENDING = 1;
+    const WEBHOOK_STATUS_ERROR = 2;
+    const WEBHOOK_STATUS_PROCESSED = 3;
+}
