1e654e11f67c8c6de93384e379cc98f92fec1210
Jan Brinkmann <jan@jbcommerce.de>
2021-03-23 11:28:17 +0100

    webhook endpoint now just records queue entries

--- Controller/Subscriber/Webhook.php
+++ Controller/Subscriber/Webhook.php
@@ -1,18 +1,54 @@
 <?php
 namespace Spod\Sync\Controller\Subscriber;
 
+use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
+use Spod\Sync\Api\ResultDecoder;
+use Spod\Sync\Model\WebhookFactory;
+use Spod\Sync\Model\WebhookStatus;
 
-class Webhook extends \Magento\Framework\App\Action\Action
+class Webhook extends Action  implements HttpPostActionInterface, CsrfAwareActionInterface
 {
+    private $decoder;
+    private $webhookFactory;
+    private $webhookRepository;
+
     public function __construct(
-        Context $context)
-    {
+        Context $context,
+        ResultDecoder $decoder,
+        WebhookFactory $webhookFactory
+    ) {
+        $this->decoder = $decoder;
+        $this->webhookFactory = $webhookFactory;
+
         return parent::__construct($context);
     }
 
     public function execute()
     {
-        echo "ff";
+        $rawJson = $this->getRequest()->getContent();
+        $responseObject = $this->decoder->parsePayload($rawJson);
+
+        $webhook = $this->webhookFactory->create();
+        $webhook->setEventType($responseObject->eventType);
+        $webhook->setStatus(WebhookStatus::WEBHOOK_STATUS_PENDING);
+        $webhook->setPayload($rawJson);
+        $webhook->save($webhook);
+
+        echo "[accepted]";
+    }
+
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
+    {
+        return null;
+    }
+
+    public function validateForCsrf(RequestInterface $request): ?bool
+    {
+        return true;
     }
 }
