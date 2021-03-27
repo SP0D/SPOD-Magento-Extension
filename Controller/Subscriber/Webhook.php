b6d041d4cddf8744447a2d46934748e42981b6ff
Jan Brinkmann <jan@jbcommerce.de>
2021-03-27 16:53:30 +0100

    save webhook events using a repository

--- Controller/Subscriber/Webhook.php
+++ Controller/Subscriber/Webhook.php
@@ -8,21 +8,28 @@ use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
 use Spod\Sync\Api\ResultDecoder;
+use Spod\Sync\Model\Repository\WebhookEventRepository;
 use Spod\Sync\Model\WebhookFactory;
 use Spod\Sync\Model\Mapping\QueueStatus;
 
 class Webhook extends Action  implements HttpPostActionInterface, CsrfAwareActionInterface
 {
+    /** @var ResultDecoder  */
     private $decoder;
+    /** @var WebhookFactory  */
     private $webhookFactory;
+    /** @var WebhookEventRepository */
+    private $webhookEventRepository;
 
     public function __construct(
         Context $context,
         ResultDecoder $decoder,
-        WebhookFactory $webhookFactory
+        WebhookFactory $webhookFactory,
+        WebhookEventRepository $webhookEventRepository
     ) {
         $this->decoder = $decoder;
         $this->webhookFactory = $webhookFactory;
+        $this->webhookEventRepository = $webhookEventRepository;
 
         return parent::__construct($context);
     }
@@ -30,24 +37,52 @@ class Webhook extends Action  implements HttpPostActionInterface, CsrfAwareActio
     public function execute()
     {
         $rawJson = $this->getRequest()->getContent();
-        $responseObject = $this->decoder->parsePayload($rawJson);
-
-        $webhook = $this->webhookFactory->create();
-        $webhook->setEventType($responseObject->eventType);
-        $webhook->setStatus(QueueStatus::STATUS_PENDING);
-        $webhook->setPayload($rawJson);
-        $webhook->save($webhook);
+        $eventType = $this->getEventTypeFromWebhookJson($rawJson);
+        $this->saveWebhookEvent($eventType, $rawJson);
 
         echo "[accepted]";
     }
 
+    /**
+     * @param RequestInterface $request
+     * @return InvalidRequestException|null
+     */
     public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
     {
         return null;
     }
 
+    /**
+     * @param RequestInterface $request
+     * @return bool|null
+     */
     public function validateForCsrf(RequestInterface $request): ?bool
     {
         return true;
+    }
+
+    /**
+     * @param $eventType
+     * @param $rawJson
+     * @throws \Exception
+     */
+    private function saveWebhookEvent($eventType, $rawJson): void
+    {
+        $webhook = $this->webhookFactory->create();
+        $webhook->setEventType($eventType);
+        $webhook->setStatus(QueueStatus::STATUS_PENDING);
+        $webhook->setPayload($rawJson);
+
+        $this->webhookEventRepository->save($webhook);
+    }
+
+    /**
+     * @param $rawJson
+     * @return mixed
+     */
+    private function getEventTypeFromWebhookJson($rawJson)
+    {
+        $responseObject = $this->decoder->parsePayload($rawJson);
+        return $responseObject->eventType;
     }
 }
