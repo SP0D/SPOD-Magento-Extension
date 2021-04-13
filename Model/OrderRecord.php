bceae215dcae8d18ae07874a9c3deaaffcda57a3
Jan Brinkmann <jan@jbcommerce.de>
2021-04-13 09:59:44 +0200

    simplify fqdn

--- Model/OrderRecord.php
+++ Model/OrderRecord.php
@@ -1,9 +1,15 @@
 <?php
 namespace Spod\Sync\Model;
 
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\DataObject\IdentityInterface;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
 use Spod\Sync\Api\ResultDecoder;
 
-class OrderRecord extends \Magento\Framework\Model\AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
+class OrderRecord extends AbstractModel implements IdentityInterface
 {
     const RECORD_EVENT_TYPE_CREATE = 'create';
     const RECORD_EVENT_TYPE_UPDATE = 'update';
@@ -18,10 +24,10 @@ class OrderRecord extends \Magento\Framework\Model\AbstractModel implements \Mag
     private $decoder;
 
     public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        Context $context,
+        Registry $registry,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
         ResultDecoder $decoder,
         array $data = [])
     {
