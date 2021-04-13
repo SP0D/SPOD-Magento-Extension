ca5a533af769f2eddc6454f6ec1af4546f64fd2b
Jan Brinkmann <jan@jbcommerce.de>
2021-04-13 09:58:08 +0200

    simplify fqdn

--- Model/ResourceModel/OrderRecord.php
+++ Model/ResourceModel/OrderRecord.php
@@ -1,9 +1,10 @@
 <?php
 namespace Spod\Sync\Model\ResourceModel;
 
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\Model\ResourceModel\Db\Context;
 
-class OrderRecord extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+class OrderRecord extends AbstractDb
 {
     public function __construct(
         Context $context
