6cbd18d12a18356a91111d5a897a498af1804351
Jan Brinkmann <jan@jbcommerce.de>
2021-04-18 10:18:26 +0200

    comments for decoder/encoder

--- Model/Decoder/JsonDecoder.php
+++ Model/Decoder/JsonDecoder.php
@@ -4,6 +4,11 @@ namespace Spod\Sync\Model\Decoder;
 
 use Spod\Sync\Api\ResultDecoder;
 
+/**
+ * Decoder implementation for the JSON format.
+ *
+ * @package Spod\Sync\Model\Decoder
+ */
 class JsonDecoder implements ResultDecoder
 {
     public function parsePayload($payload)
