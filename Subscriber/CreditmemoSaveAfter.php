fdcf81723a44fcb1cc4d71bd618a1070c8324a61
Ruslan Voitenko <ruslan.voitenko@trilix-gmbh.de>
2021-12-20 12:27:07 +0100

    Adds notification to creditmemo if order contains SPOD products

---
+++ Subscriber/CreditmemoSaveAfter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Spod\Sync\Subscriber;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Api\Data\CreditmemoInterface;
+use Magento\Sales\Api\Data\OrderItemInterface;
+use Magento\Sales\Model\Order\Creditmemo;
+
+class CreditmemoSaveAfter implements ObserverInterface
+{
+    public function execute(Observer $observer)
+    {
+        /** @var CreditmemoInterface|Creditmemo $creditmemo */
+        $creditmemo = $observer->getCreditmemo();
+        $order = $creditmemo->getOrder();
+
+        if (!$order->getSpodOrderId() || $order->getSpodCancelled()) {
+            return;
+        }
+
+        $spodOrderItems = array_filter(
+            $order->getItems(),
+            function (OrderItemInterface $orderItem) {
+                return $orderItem->getData('spod_order_item_id') && $orderItem->getQtyToInvoice() == 0;
+            }
+        );
+
+        if (count($spodOrderItems)) {
+            $creditmemo->addComment(
+                'Please, contact SPOD customer support. Current order has SPOD products and was synced to SPOD system.',
+                false,
+                false
+            );
+            $order->addCommentToStatusHistory(
+                sprintf('Please, contact SPOD customer service due to Creditmemo %s', $creditmemo->getIncrementId()),
+                false,
+                false
+            );
+        }
+    }
+}
